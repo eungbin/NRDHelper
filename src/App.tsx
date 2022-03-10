a9ae76f62da382dc50421954d3d73b6ac3a58554
eungbin <programmer97@naver.com>
2022-03-10 10:53:44 +0900

    헤더 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 interface Props {}
 
 const App = ({  }: Props) => {
   return (
-    <div className="container">
-      <h1>Hello, World!</h1>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <h1>Hello, World!</h1>
+      </div>
+    </BrowserRouter>
   );
 };
 
