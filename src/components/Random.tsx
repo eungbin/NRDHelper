34cf702cad40d5ab233a7235fb92f3a0aefcdb48
eungbin <programmer97@naver.com>
2022-03-28 19:25:13 +0900

    Random.tsx 작성중

--- src/components/Random.tsx
+++ src/components/Random.tsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react';
 import '../css/Random.css';
 import { useLocation } from 'react-router-dom';
 
+interface props {
+  user1: string;
+  user2: string;
+}
+
 const Random = () => {
   const location = useLocation();
+  const users = location.state as props   // 그냥 location.state.user1로 사용할 경우 에러 발생
 
   console.log(location.state);
 
   return (
     <div className="randomContainer">
-      TEST
+      {users.user1}
     </div>
   );
 };
