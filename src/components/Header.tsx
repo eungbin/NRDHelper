3cf22ad1f1f427c1e3ae27f51ea875281ad913de
eungbin <programmer97@naver.com>
2022-04-18 16:58:57 +0900

    상단 메뉴바 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="menu">
-      <Link to="/"><div className="menu-item">Home</div></Link>
+      <Link to="/"><div className="menu-item home">NRD Helper</div></Link>
     </div>
   );
 };
